c1e89f25ab5ffe867d94d97227eec6e41fa8a461
Sandeep Behera <thesbcode@gmail.com>
2025-09-13 14:43:57 +0530

    New_Version

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { FiMenu } from 'react-icons/fi';
+import './DashboardLayout.css';
 
 const DashboardLayout = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="flex min-h-screen">
-      <Navbar />
-      <main className="flex-grow p-8 bg-gray-50">
+    <div className="dashboard-layout">
+      {/* Mobile Toggle Button */}
+      <button
+        className="menu-toggle"
+        onClick={() => setSidebarOpen(!isSidebarOpen)}
+      >
+        <FiMenu size={22} />
+      </button>
+
+      {/* Sidebar */}
+      <Navbar isOpen={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+      {/* Main Content */}
+      <main className="dashboard-main">
         <Outlet />
       </main>
     </div>
